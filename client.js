988437b9f4ded20a754a356ea4dcba40ec6912fe
santoshi746 <111972093+santoshi746@users.noreply.github.com>
2022-11-30 20:44:15 +0530

    client

--- client.js
+++ client.js
@@ -1,18 +1,30 @@
 function sign(){
-
-    var FName = document.getElementById("iii");
-    var Mobile = document.getElementById("jjj");
-    var EmailAddress = document.getElementById("kkk");
-    var UserName = document.getElementById("lll");
+    var Name = document.getElementById("iii");
+    var Number = document.getElementById("jjj");
+    var EmailAddress= document.getElementById("kkk");
+    var UserName= document.getElementById("lll");
     var Password = document.getElementById("mmm");
 
-
-    if(FName.value.trim() =="" || Mobile.value.trim()=="" || EmailAddress.value.trim()=="" || UserName.value.trim()=="" || Password.value.trim() == "" )
-    {
-           alert("credentials are mandatory");
-           return false;
+    if(Name.value.trim()=="" || Number.value.trim()=="" || EmailAddress.value.trim()==""|| UserName.value.trim()=="" || Password.value.trim()==""){
+        alert("No empty values are allowed");
+        return false;
     }
     else{
+        alert("Successfully signed in");
+        true;
+    }
+}
+
+function log(){
+    var LogName = document.getElementById("nnn");
+    var LogPswd = document.getElementById("ooo");
+
+    if(LogName.value.trim()=="" || LogPswd.value.trim()==""){
+        alert("No empty values are allowed");
+        return false;
+    }
+    else{
+        alert("Successfully logged in");
         true;
     }
 }
